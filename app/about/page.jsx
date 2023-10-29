27fce6a1140b6d66d400446f3261ca323ce33740
John Erwin <jerwin@penlink.com>
2023-10-29 14:54:29 -0600

    about page mobile first redesign

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,27 +2,54 @@ import TapedImage from "@components/TapedImage";
 
 const About = () => {
   return (
-    <div className="flex flex-col items-center p-10 border border-red-400">
-      <div className="xl:grid xl:grid-cols-12 flex flex-col-reverse items-center max-w-screen-lg border border-black w-fit m-10 p-5">
-        <div className="xl:col-span-6 border  xl:lg:mt-20  border-blue-400 ">
-          <p className="text-4xl">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type{" "}
-            <span className="red-highlight-container">
-              <span className="highlight"> University of Nebraska-Lincoln</span>
-            </span>{" "}
-            specimen book. It has survived not only five centuries, but also the
-            leap into electronic typesetting, remaining essentially unchanged.
-            It was popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.
+    <div className="hand-drawn-border flex flex-col items-center ">
+      About
+      <div className="flex flex-col items-center w-[95%] border border-green-400">
+        the stuff goes here
+        <TapedImage imageUrl={"/assets/johnAndBassWBackgroundLandscape.jpg"} />
+        <div className="flex flex-col text-md sm:text-lg leading-tight font-semibold mt-4 space-y-2">
+          <p>
+            <span className="text-2xl">Hello! I'm John Erwin</span>, <br /> a
+            full-stack software engineer and graduate of{" "}
+            <span className="text-red-700">
+              The University of Nebraska-Lincoln
+            </span>
+            , with a degree in computer science and minors in business and
+            mathematics.
           </p>
-        </div>
-        <div className="col-span-6 sm:max-w-screen-sm self-start  border border-green-400">
-          <TapedImage imageUrl="/assets/JohnAndBassWBackground.jpg" />
+
+          <p>
+            It was there that I met my good friend and introduction the world of
+            writing code, Ben Morris. Since then, I have an enjoyed an exciting
+            an adventure into the world of software engineering full of
+            learning, several success, and several more failures.
+          </p>
+          <p>
+            I entered the computer science department severely lacking in
+            technical skills, but fortunately I had little fear of asking
+            "stupid" questions, and by leaning on my people skills and friends
+            like Ben and Tanner Skelton I was able to quickly improve my
+            technical weaknesses. Now that I've become more proficient in the
+            technical aspects of software engineering, I'm excited to see what
+            sort of positive impact I can make on the world with software.
+          </p>
+          <p>
+            I've continued to hone my skills through experiences at work, and
+            with personal and collaborative projects on the side. I don't mind
+            working alone, but I enjoy working with others the most. Group work
+            has always been a fun way to learn and accomplish what I wouldn't be
+            able to on my own.
+          </p>
+          <p>
+            On a more personal note, I grew up in a small town called York,
+            Nebraska as the oldest child in a large family. Today I reside in
+            Boulder, Colorado. When I'm not writing code, it might be hard to
+            find me. I'm a passionate outdoorsman, often getting my boots wet
+            and (if I'm lucky) my hands slimy while fly fishing. During the
+            colder months, you might find me hurdling down a mountain on a
+            snowboard or sitting on a frozen lake, again, trying to get my hands
+            a little slimy with some ice fishing.
+          </p>
         </div>
       </div>
     </div>
